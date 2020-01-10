aacb1af78b4ae6480d513fa7ea9bdda2dbc30ff0
Oles Marola <oles_pw@ukr.net>
2020-01-10 13:50:43 +0200

    Updates

---
+++ precache-manifest.5280ad384d6a18bee3d6b7a17d9862f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc02f63ea2de9ff8d20c3582fc199c8",
+    "url": "/test_task-users_with_pagination/index.html"
+  },
+  {
+    "revision": "fd2be9393c82d6235287",
+    "url": "/test_task-users_with_pagination/static/css/main.8a7ea5ea.chunk.css"
+  },
+  {
+    "revision": "20b353ff3df692b499bb",
+    "url": "/test_task-users_with_pagination/static/js/2.9aa4f75e.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/test_task-users_with_pagination/static/js/2.9aa4f75e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd2be9393c82d6235287",
+    "url": "/test_task-users_with_pagination/static/js/main.f75036fc.chunk.js"
+  },
+  {
+    "revision": "0189f9a6e7ff04078e35",
+    "url": "/test_task-users_with_pagination/static/js/runtime-main.876b6e53.js"
+  }
+]);
